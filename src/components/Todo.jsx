4e94906c1610619adebb6b1e9ed91d88023db871
Dmitriy Sidorov <dmitrii.sidorov@accenture.com>
2020-03-17 22:45:39 +0300

    state, setState

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,11 +5,27 @@ import Button from './Button';
 import Checkbox from './Checkbox';
 
 class Todo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            completed: this.props.completed
+        }
+    }
+
+    handleClick(event) {
+        this.setState({
+            completed: !this.state.completed
+        })
+    }
+
     render() {
         return (
-            <div className={`todo ${this.props.completed ? 'completed' : ''}`}>
-                <Checkbox checked={this.props.completed}/>
-                <span className="todo__title">{this.props.title}</span>
+            <div className={`todo ${this.state.completed ? 'completed' : ''}`}>
+                <div className="todo__item" onClick={() => this.handleClick()}>
+                    <Checkbox checked={this.state.completed}/>
+                    <span className="todo__title">{this.props.title}</span>
+                </div>
                 <Button className={'todo__delete icon'} icon={'delete'}/>
             </div>
         );
