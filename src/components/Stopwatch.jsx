dedb23d04fd202b0968e68013dd3a30fe7f0c312
Dmitriy Sidorov <dmitrii.sidorov@accenture.com>
2020-03-31 11:21:43 +0300

    stopwatch

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -8,17 +8,88 @@ class Stopwatch extends Component {
         super(props);
 
         this.state = {
-            running: false
+            running: false,
+            elapsed: 0,
+            lastTick: 0
         };
+
+        this.tick = this.tick.bind(this);
+        this.handleStart = this.handleStart.bind(this);
+        this.handlePause = this.handlePause.bind(this);
+        this.handleStop = this.handleStop.bind(this);
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.tick, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
+    tick() {
+        if(this.state.running) {
+            let now = Date.now();
+            let diff = now - this.state.lastTick;
+
+            this.setState({
+                elapsed: this.state.elapsed + diff,
+                lastTick: now
+            })
+        }
+    }
+
+    handleStart() {
+        this.setState({
+            running: true,
+            lastTick: Date.now()
+        })
+    }
+
+    handlePause() {
+        this.setState({running: false})
+    }
+
+    handleStop() {
+        this.setState({
+            running: false,
+            elapsed: 0,
+            lastTick: 0
+        })
+    }
+
+    mask(array) {
+        let arraySize = array.length;
+        let format = '';
+        return array.map((item, i) => {
+            let mask = item > 9 ? item : '0' + item;
+            return format =  arraySize === i+1 ? mask : mask + ':';
+        })
+    }
+
+    format(milliseconds) {
+        let totalSeconds = Math.floor(milliseconds / 1000);
+        let totalMinutes = Math.floor(totalSeconds / 60);
+        let hours = Math.floor(totalMinutes / 60);
+        let minutes = totalMinutes % 60;
+        let seconds = totalSeconds % 60;
+
+        return this.mask([hours,minutes,seconds])
     }
 
     render() {
+        let time = this.format(this.state.elapsed);
+
         return (
             <div className="stopwatch">
-                <div className="stopwatch__time">00:00:00</div>
+                <div className="stopwatch__time">{time}</div>
                 <div className="stopwatch__controls">
-                    <Button className="stopwatch__icon" icon={this.state.running ? 'pause' : 'play_arrow'}/>
-                    <Button className="stopwatch__icon" icon="stop"/>
+                    {this.state.running ?
+                        <Button className="stopwatch__icon" icon="pause" onClick={this.handlePause}/>
+                        :
+                        < Button className = "stopwatch__icon" icon="play_arrow" onClick={this.handleStart}/>
+                    }
+                    <Button className="stopwatch__icon" icon="stop" onClick={this.handleStop}/>
                 </div>
             </div>
         );
